af2e26d0d6e37f667e98bacdefd46b81d89e28f7
Arqavan <jannati.leila99@gmail.com>
2024-01-26 19:33:52 +0330

    SQLALCHEMY ADDED - ZIBAEI

--- model/entity/visit.py
+++ model/entity/visit.py
@@ -1,8 +1,16 @@
+from sqlalchemy import Column, Integer, String
+
 from model.entity.base import Base
 
 
 class Visit(Base):
     __tablename__ = "visit_tbl"
+
+    patient = Column(Integer(30))
+    timing = Column(String(20))
+    visit_time = Column(String(20))
+    duration = Column(String(20))
+    payment = Column(String(20))
 
     def __init__(self, patient, timing, visit_time, duration, payment):
         self.patient = patient
