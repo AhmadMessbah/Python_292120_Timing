ef3ac3f66b760e341555afcd13a9b03101b72c77
Atyabi <fahimeaty1379@gmail.com>
2024-01-26 16:32:27 +0330

    Timing Updated - Atyabi

--- model/entity/timing.py
+++ model/entity/timing.py
@@ -1,3 +1,8 @@
 class Timing:
     def __init__(self, timing_date, start_time, end_time):
-        pass
+        self.timing_date = timing_date
+        self.start_time = start_time
+        self.end_time = end_time
+
+    def __repr__(self):
+        return str(self.__dict__)
