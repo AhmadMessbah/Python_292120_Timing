d1e60352a8395337ba7f10b09fbcec839d0ee312
Arqavan <jannati.leila99@gmail.com>
2024-01-26 16:40:10 +0330

    Visit Updated - Zibaei

--- model/entity/visit.py
+++ model/entity/visit.py
@@ -1,7 +1,11 @@
 class Visit:
     def __init__(self, patient, timing, visit_time, duration, payment):
+        self.id = None
         self.patient = patient
         self.timing = timing
         self.visit_time = visit_time
         self.duration = duration
         self.payment = payment
+
+    def __repr__(self):
+        return str(self.__dict__)
