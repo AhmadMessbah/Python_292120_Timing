9fd0053221b1b82aaa8f79716271b0579f22b290
amirmahdi <amirmahdi.kian003@gmail.com>
2024-02-02 17:25:48 +0330

    PatientController / MedicalServiceController/MedicalServiceDa/  patient_da- shojaeikian

--- model/da/patient_da.py
+++ model/da/patient_da.py
@@ -1,5 +1,25 @@
 from model.da.database_manager import DatabaseManager
+from model.entity.patient import Patient
 
 
 class PatientDa(DatabaseManager):
-    pass
+
+    def find_by_family(self, family):
+        self.make_engine()
+        entity = self.session.query(Patient).filter_by(family=family)
+        return entity
+
+    def find_by_national_id(self, national_id):
+        self.make_engine()
+        entity = self.session.query(Patient).filter_by(national_id=national_id)
+        return entity
+
+    def find_by_username(self, username):
+        self.make_engine()
+        entity = self.session.query(Patient).filter_by(username=username)
+        return entity
+
+    def find_by_username_password(self, username, password):
+        self.make_engine()
+        entity = self.session.query(Patient).filter_by(username=username, password=password)
+        return entity
