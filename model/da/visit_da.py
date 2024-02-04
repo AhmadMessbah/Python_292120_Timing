41b4d1d5bf78977782312d20e3ccc1729ba7daf7
Arqavan <jannati.leila99@gmail.com>
2024-02-04 16:18:51 +0330

    visit_da Updated Zibaei

--- model/da/visit_da.py
+++ model/da/visit_da.py
@@ -1,5 +1,9 @@
-from model.da.database_manager import DatabaseManager
+from Python_292120_Timing.model.da.database_manager import DatabaseManager
+from Python_292120_Timing.model.entity.visit import Visit
 
 
 class VisitDa(DatabaseManager):
-    pass
+    def find_by_timing(self, timing):
+        self.make_engine()
+        result = self.session.query(Visit).filter(Visit.timing == timing)
+        return result
