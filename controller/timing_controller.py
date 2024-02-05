f4654263741ec85c831f79a675adc19fbb45063a
Atyabi <fahimeaty1379@gmail.com>
2024-02-05 22:36:39 +0330

    timing_controller Updated - Atyabi

--- controller/timing_controller.py
+++ controller/timing_controller.py
@@ -1,6 +1,6 @@
 from model.da.timing_da import TimingDa
 from model.entity.timing import Timing
-from model.tools.validation import date_validator,time_validator
+from model.tools.validation import *
 import tkinter.messagebox as msg
 class TimingController:
     def save(self, date, start_time, end_time, status=True):
@@ -29,7 +29,13 @@ class TimingController:
             msg.showerror("error",f"error : {e}")
 
     def remove(self, id):
-        pass
+        try:
+            da = TimingDa()
+            da.remove(id)
+            return "deleted"
+
+        except Exception as e:
+            msg.showerror("error",f"error : {e}")
 
 
     def find_all(self):
@@ -37,14 +43,50 @@ class TimingController:
 
 
     def find_by_date(self, date):
-       pass
+        try:
+            if date_validator(date, "error"):
+                da = TimingDa()
+                result = da.find_by_date(date)
+                msg.showinfo("info", str(result))
+
+                return "found"
+
+        except Exception as e:
+            msg.showerror("error",f"error : {e}")
 
 
     def find_by_start_time(self, start_time):
-        pass
+        try:
+            if date_validator(start_time, "error"):
+                da = TimingDa()
+                result = da.find_by_start_time(start_time)
+                msg.showinfo("info", str(result))
+
+                return "found"
+
+        except Exception as e:
+            msg.showerror("error", f"error : {e}")
 
     def find_by_end_time(self, end_time):
-        pass
+        try:
+            if date_validator(end_time, "error"):
+                da = TimingDa()
+                result = da.find_by_end_time(end_time)
+                msg.showinfo("info", str(result))
+
+                return "found"
+
+        except Exception as e:
+            msg.showerror("error", f"error : {e}")
 
     def find_by_doctor_id(self, doctor_id):
-        pass
+        try:
+            if date_validator(doctor_id, "error"):
+                da = TimingDa()
+                result = da.find_by_doctor_id(doctor_id)
+                msg.showinfo("info", str(result))
+
+                return "found"
+
+        except Exception as e:
+            msg.showerror("error", f"error : {e}")
