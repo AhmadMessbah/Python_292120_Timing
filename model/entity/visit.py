b1a76839e232271efaf3ab01f20462018c5b3e9e
Arqavan <jannati.leila99@gmail.com>
2024-02-08 20:45:12 +0330

    visit.py Updated Zibaei

--- model/entity/visit.py
+++ model/entity/visit.py
@@ -1,20 +1,20 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 
-from model.entity.base import Base
+from Python_292120_Timing.model.entity.base import Base
 
 
 class Visit(Base):
     __tablename__ = "visit_tbl"
     id = Column(Integer, primary_key=True)
-    patient = Column(Integer, ForeignKey("patient_tbl.id"))
-    timing = Column(String(30))
+    patient_id = Column(Integer, ForeignKey("patient_tbl.id"))
+    timing_id = Column(Integer, ForeignKey("timing_tbl.id"))
     visit_time = Column(String(20))
     duration = Column(String(20))
     payment = Column(String(20))
 
-    def __init__(self, patient, timing, visit_time, duration, payment):
-        self.patient = patient
-        self.timing = timing
+    def __init__(self, patient_id, timing_id, visit_time, duration, payment):
+        self.patient_id = patient_id
+        self.timing_id = timing_id
         self.visit_time = visit_time
         self.duration = duration
         self.payment = payment
