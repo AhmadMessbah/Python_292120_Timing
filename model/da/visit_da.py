c3e47a9d7416209833a52697197114f7b93ae503
Arqavan <jannati.leila99@gmail.com>
2024-02-08 20:46:13 +0330

    visit_da Updated Zibaei

--- model/da/visit_da.py
+++ model/da/visit_da.py
@@ -3,7 +3,17 @@ from Python_292120_Timing.model.entity.visit import Visit
 
 
 class VisitDa(DatabaseManager):
-    def find_by_timing(self, timing):
+    def find_by_timing_id(self, timing_id):
         self.make_engine()
-        result = self.session.query(Visit).filter(Visit.timing == timing)
+        result = self.session.query(Visit).filter(Visit.timing_id == timing_id)
+        return result
+
+    def find_by_visit_time(self, visit_time):
+        self.make_engine()
+        result = self.session.query(Visit).filter(Visit.visit_time == visit_time)
+        return result
+
+    def find_by_patient_id(self, patient_id):
+        self.make_engine()
+        result = self.session.query(Visit).filter(Visit.patient_id == patient_id)
         return result
