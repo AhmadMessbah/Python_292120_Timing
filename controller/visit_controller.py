90fb4817e62c9fbd19e1a7211ba861dc88b5e8cc
Arqavan <jannati.leila99@gmail.com>
2024-02-11 19:33:33 +0330

    visit_controller.py Updated Zibaei

--- controller/visit_controller.py
+++ controller/visit_controller.py
@@ -3,53 +3,69 @@ from model.entity.visit import Visit
 
 
 class VisitController:
-    def __init__(self):
-        self.da = VisitDa()
-
-    def save(self, patient_id, timing_id, visit_time, duration, payment):
+    @classmethod
+    def save(cls, patient, timing, visit_time, duration, payment):
         try:
-            visit = Visit(patient_id, timing_id, visit_time, duration, payment)
-            self.da.save(visit)
-            return True
+            visit = Visit(patient, timing, visit_time, duration, payment)
+            da = VisitDa()
+            da.save(visit)
+            return visit
         except Exception as e:
             return e
 
-    def edit(self, id, patient, visit_time):
+    @classmethod
+    def edit(cls, id, visit_time, duration, payment):
         try:
-            visit = VisitDa(patient, visit_time)
-            visit.id = id
-            self.da.edit(visit)
-            return True
-        except Exception as e:
-            return e
+            da = VisitDa()
+            visit = da.find_by_id(Visit, id)
 
-    def remove(self, id):
+            if visit:
+                visit.visit_time = visit_time
+                visit.duration = duration
+                visit.payment = payment
+                result = da.edit(visit)
+                print(result)
+
+        except Exception as e:
+            print(e)
+
+    @classmethod
+    def remove(cls, id):
         try:
-            self.da.remove(id)
+            da = VisitDa()
+            da.remove(id)
             return "deleted"
         except Exception as e:
             return e
 
-    def find_by_id(self, id):
+    @classmethod
+    def find_by_id(cls, id):
         try:
-            return self.da.find_by_id(id)
+            da = VisitDa()
+            return da.find_by_id(id)
         except Exception as e:
             return e
 
-    def find_by_timing_id(self, timing_id):
+    @classmethod
+    def find_by_timing_id(cls, timing_id):
         try:
-            return self.da.find_by_timing_id(timing_id)
+            da = VisitDa()
+            return da.find_by_timing_id(timing_id)
         except Exception as e:
             return e
 
-    def find_by_visit_time(self, visit_time):
+    @classmethod
+    def find_by_visit_time(cls, visit_time):
         try:
-            return self.da.find_by_visit_time(visit_time)
+            da = VisitDa()
+            return da.find_by_visit_time(visit_time)
         except Exception as e:
             return e
 
-    def find_by_patient_id(self, patient_id):
+    @classmethod
+    def find_by_patient_id(cls, patient_id):
         try:
-            return self.da.find_by_patient_id(patient_id)
+            da = VisitDa()
+            return da.find_by_patient_id(patient_id)
         except Exception as e:
             return e
