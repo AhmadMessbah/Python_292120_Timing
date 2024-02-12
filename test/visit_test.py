0c4bccdd100bfa391414e00e10c6468c438b5173
Arqavan <jannati.leila99@gmail.com>
2024-02-12 11:59:49 +0330

    visit_test.py Updated Zibaei

--- test/visit_test.py
+++ test/visit_test.py
@@ -2,7 +2,6 @@
 # from controller.visit_controller import VisitController
 # from controller.patient_controller import PatientController
 
-
 # patient = PatientController()
 # p1 = patient.find_by_id(3)
 # print(p1)
@@ -10,25 +9,28 @@
 # timing = TimingController()
 # t1 = timing.find_by_id(3)
 # print(t1)
-#
 # da = VisitController()
-# visit = da.save(p1,t1,"12:30","00:30","300000")
+# visit = da.save(p1, t1, "12:30", "00:30", "300000")
 # print(visit)
 
-from controller.medical_service_controller import MedicalServiceController
-from controller.doctor_controller import DoctorController
-from controller.timing_controller import TimingController
-from controller.patient_controller import PatientController
-from controller.visit_controller import VisitController
 
-medical = MedicalServiceController()
-doctor = DoctorController()
-timing = TimingController()
-patient = PatientController()
+# from controller.medical_service_controller import MedicalServiceController
+# from controller.doctor_controller import DoctorController
+# from controller.timing_controller import TimingController
+# from controller.visit_controller import VisitController
+# from controller.patient_controller import PatientController
 
-medical_result = medical.save("jarah", "ghalb")
-doctor_result = doctor.save("diba", "mahmoudi", "5874216938", "1341.05.11", "8756324589", "dibaaaa", "mahboob", "jarah",
-                            medical_result)
-timing_result = timing.save("1402.12.03", "8:30", "10:30", doctor_result)
-patient_result = patient.save("hanie", "mehri", "5462713265", "1385.06.12", "09123658791", "haniii", "mehriii")
-VisitController.save(patient_result, timing_result, "16:00", "sick", "50000")
+# medical = MedicalServiceController()
+# doctor = DoctorController()
+# timing = TimingController()
+# patient = PatientController()
+# visit = VisitController()
+
+# medical_result = medical.save("jarahi", "dakheli")
+# doctor_result = doctor.save("ali", "vahidi", "6387912458", "1357.07.117", "81061125479", "vahiiid", "aliii", "jarah",
+# medical_result)
+# t1 = timing.save("1402.06.10", "8:30", "11:00", doctor_result)
+# p1 = patient.save("sina", "ramezani", "1236548974", "1385.06.12", "09123658791", "siiin", "raaam")
+# visit_controller = visit.save(p1, t1, "16:00", "00:30", "1400000")
+# print(visit_controller)
+
