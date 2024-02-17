b92aac79499f195f9c5be9e9dc66c45f63523628
Arqavan <jannati.leila99@gmail.com>
2024-02-17 10:27:12 +0330

    visit_controller.py Updated Zibaei

--- controller/visit_controller.py
+++ controller/visit_controller.py
@@ -3,25 +3,27 @@ from model.entity.visit import Visit
 
 
 class VisitController:
+    def __init__(self):
+        self.da = VisitDa()
+
     def save(self, patient, timing, visit_time, duration, payment):
         try:
             visit = Visit(patient, timing, visit_time, duration, payment)
-            da = VisitDa()
-            da.save(visit)
-            return visit
+            self.da.save(visit)
+            return f"{visit} saved"
         except Exception as e:
+            e.with_traceback()
             return e
 
     def edit(self, id, visit_time, duration, payment):
         try:
-            da = VisitDa()
-            visit = da.find_by_id(Visit, id)
+            visit = self.da.find_by_id(Visit, id)
 
             if visit:
                 visit.visit_time = visit_time
                 visit.duration = duration
                 visit.payment = payment
-                result = da.edit(visit)
+                result = self.da.edit(visit)
                 print(result)
 
         except Exception as e:
@@ -29,36 +31,32 @@ class VisitController:
 
     def remove(self, id):
         try:
-            da = VisitDa()
-            da.remove(id)
+            self.da.remove(id)
             return "deleted"
         except Exception as e:
             return e
 
     def find_by_id(self, id):
         try:
-            da = VisitDa()
-            return da.find_by_id(id)
+            return self.da.find_by_id(id)
         except Exception as e:
             return e
 
     def find_by_timing_id(self, timing_id):
         try:
-            da = VisitDa()
-            return da.find_by_timing_id(timing_id)
+            return self.da.find_by_timing_id(timing_id)
         except Exception as e:
             return e
 
     def find_by_visit_time(self, visit_time):
         try:
-            da = VisitDa()
-            return da.find_by_visit_time(visit_time)
+
+            return self.da.find_by_visit_time(visit_time)
         except Exception as e:
             return e
 
     def find_by_patient_id(self, patient_id):
         try:
-            da = VisitDa()
-            return da.find_by_patient_id(patient_id)
+            return self.da.find_by_patient_id(patient_id)
         except Exception as e:
             return e
